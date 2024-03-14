292bd2c2c7e7f9a5216f8a5db1318253572bde80
Carlos Abreu <crenato@minsait.com>
2024-03-14 00:06:54 -0300

    [MAIN]
       -ajustes na controller de curso
    .

--- src/main/java/com/example/springbootEssentials/SpringbootEssentialsApplication.java
+++ src/main/java/com/example/springbootEssentials/SpringbootEssentialsApplication.java
@@ -1,14 +1,38 @@
 package com.example.springbootEssentials;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.example.springbootEssentials.model.Curso;
+import com.example.springbootEssentials.model.CursoRepository;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication(scanBasePackages = {"com.example"})
+@Slf4j
 public class SpringbootEssentialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+		@Bean
+	public CommandLineRunner loadDatabase(CursoRepository repository) {
+	log.info("Loading data...");
+		if (repository.count() > 0) {
+			return (args) -> {
+			log.info("Database is not empty");
+		};   
+	}
+		return (args) -> {
+			log.info("Criando Database");
+			repository.save(new Curso(1l, "Curso 1", 6));
+			repository.save(new Curso(2l, "Curso 2", 4));
+			repository.save(new Curso(3l, "Curso 3", 20));
+			};
 	}
 }
--- src/main/java/com/example/springbootEssentials/controller/CursoController.java
+++ src/main/java/com/example/springbootEssentials/controller/CursoController.java
@@ -2,15 +2,25 @@ package com.example.springbootEssentials.controller;
 
 
 import java.util.List;
+import java.util.Objects;
 import java.util.ArrayList;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+
 import com.example.springbootEssentials.model.Curso;
 import com.example.springbootEssentials.model.CursoRepository;
 
+import jakarta.websocket.server.PathParam;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -23,13 +33,47 @@ public class CursoController {
     @GetMapping()
     public List<Curso> list() {
         log.info("Listando os cursos.");
-        /*List<Curso> cursos = new ArrayList<>();
-        Curso curso = new Curso();
-        curso.setTitulo("curso1");
-        curso.setCreditos(6);
-        cursos.add(curso);*/
-
-        return cursoRepository.findAll();
+                 return cursoRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Curso get(@PathVariable("id") Long id) throws BadRequestException{
+        if(!Objects.isNull(id)){
+            log.info("Listando o curso: {}",id);
+            
+        }else{
+            throw new BadRequestException();
+        }
+        return cursoRepository.findById(id).get();
+    }
+    @PostMapping()
+    public Curso save(@RequestBody Curso curso)  throws BadRequestException{
+        if(!Objects.isNull(curso.getId())){
+            log.info("Atualiza o curso: {}",curso);
+            
+        }else{
+            throw new BadRequestException();
+        }
+        return cursoRepository.save(curso);
+    }
+    @PatchMapping("/{id}")
+    public Curso update(@PathVariable ("id") Long id, @RequestBody Curso curso) throws BadRequestException{
+        if(!Objects.isNull(curso.getId())){
+            log.info("Atualiza o curso: {}",curso);
+            
+        }else{
+            throw new BadRequestException();
+        }
+        return cursoRepository.save(curso);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable ("id") Long id)  throws BadRequestException{
+        if(!Objects.isNull(id)){
+            log.info("Deleta o curso: {}",id);
+            
+        }else{
+            throw new BadRequestException();
+        }
+        cursoRepository.deleteById(id);
     }
 
 }
